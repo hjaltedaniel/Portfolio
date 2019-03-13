f3386a859dd7a22e26b1eb3fdbdc8e11cb9e065a
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-03-13 17:25:21 +0100

    Instansiating npm packages

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import bulma from 'bulma'
+import lodash from 'lodash'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  bulma,
+  lodash,
   render: h => h(App)
 }).$mount('#app')
