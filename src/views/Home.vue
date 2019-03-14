d1f46afe038ca71fe0602b26e91c34edfbde026e
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-03-14 11:44:03 +0100

    Introducing loading spinner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -45,16 +45,19 @@
       <section id="home" class="hero is-white is-fullheight">
         <div class="hero-body">
           <div class="container has-text-centered">
-            <div class="columns">
-              <div class="column is-half is-offset-one-quarter">
-                <h1 class="subtitle is-2 has-text-weight-light">Der var et problem med at hente data</h1>
-                <h3
-                  class="subtitle is-3 has-text-justified has-text-weight-light"
-                >Så tjek konsollen - eller vend tilbage en anden dag!</h3>
-                <h4
-                  class="subtitle is-4 has-text-weight-light"
-                >Skal du bare have fat i mig? Send en mail på hej@hjaltedaniel.io</h4>
-              </div>
+            <div class="lds-spinner">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
             </div>
           </div>
         </div>
@@ -97,7 +100,8 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "@/scss/_vars.scss";
 html,
 body {
   font-family: "GT Eesti Display", -apple-system, BlinkMacSystemFont, "Segoe UI",
@@ -130,5 +134,86 @@ body {
 .arrow {
   text-align: center;
   margin: 3% 0;
+}
+
+//Loading spinner stuff
+
+.lds-spinner {
+  color: $red;
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.lds-spinner div {
+  transform-origin: 32px 32px;
+  animation: lds-spinner 1.2s linear infinite;
+}
+.lds-spinner div:after {
+  content: " ";
+  display: block;
+  position: absolute;
+  top: 3px;
+  left: 29px;
+  width: 5px;
+  height: 14px;
+  border-radius: 20%;
+  background: $red;
+}
+.lds-spinner div:nth-child(1) {
+  transform: rotate(0deg);
+  animation-delay: -1.1s;
+}
+.lds-spinner div:nth-child(2) {
+  transform: rotate(30deg);
+  animation-delay: -1s;
+}
+.lds-spinner div:nth-child(3) {
+  transform: rotate(60deg);
+  animation-delay: -0.9s;
+}
+.lds-spinner div:nth-child(4) {
+  transform: rotate(90deg);
+  animation-delay: -0.8s;
+}
+.lds-spinner div:nth-child(5) {
+  transform: rotate(120deg);
+  animation-delay: -0.7s;
+}
+.lds-spinner div:nth-child(6) {
+  transform: rotate(150deg);
+  animation-delay: -0.6s;
+}
+.lds-spinner div:nth-child(7) {
+  transform: rotate(180deg);
+  animation-delay: -0.5s;
+}
+.lds-spinner div:nth-child(8) {
+  transform: rotate(210deg);
+  animation-delay: -0.4s;
+}
+.lds-spinner div:nth-child(9) {
+  transform: rotate(240deg);
+  animation-delay: -0.3s;
+}
+.lds-spinner div:nth-child(10) {
+  transform: rotate(270deg);
+  animation-delay: -0.2s;
+}
+.lds-spinner div:nth-child(11) {
+  transform: rotate(300deg);
+  animation-delay: -0.1s;
+}
+.lds-spinner div:nth-child(12) {
+  transform: rotate(330deg);
+  animation-delay: 0s;
+}
+@keyframes lds-spinner {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 }
 </style>
