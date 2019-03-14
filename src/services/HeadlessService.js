a47731bf83b414244f4f564c4ba47f728934bb01
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-03-14 10:57:10 +0100

    Update HeadlessService.js

--- src/services/HeadlessService.js
+++ src/services/HeadlessService.js
@@ -3,7 +3,11 @@ import UmbracoHeadless from 'umbraco-headless'
 const headlessService = new UmbracoHeadless.HeadlessService({
     url: window.location.href,
     username: "hej@hjaltedaniel.io",
-    password: "IsQuiz5443!!"
+    password: "IsQuiz5443!!",
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    }
 })
 
 export default {
